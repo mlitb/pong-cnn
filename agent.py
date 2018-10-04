76233a7052e5c137c9e917587d3203e30f9c1e3e
Faza Fahleraz <ffahleraz@gmail.com>
2018-10-04 09:39:26 +0700

    Refactor agent with new API

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 """
-    An implementation of a policy gradient agent that learns to play Atari games.
+    A policy gradient agent with convolutional neural network.
+
     More Info : https://github.com/mlitb/pong-cnn
     Authors:
         1. Faza Fahleraz https://github.com/ffahleraz
@@ -8,36 +9,42 @@
 """
 
 import gym
+import numpy as np
+import tensorflow as tf
+from typing import Tuple
 
 
 class Agent:
 
-    def sample_action(state):
+    def __init__(self, input_shape: Tuple[int, int, int]):
+        self.conv1 = tf.keras.layers.Conv2D(input_shape=input_shape, filters=16, 
+                kernel_size=8, strides=4, activation='relu', data_format="channels_last")
+        self.conv2 = tf.keras.layers.Conv2D(filters=32, kernel_size=4, strides=2, 
+                activation='relu')
+        self.flatten = tf.keras.layers.Flatten()
+        self.fc = tf.keras.layers.Dense(256, activation='relu')
+        self.prob = tf.keras.layers.Dense(1, activation='sigmoid')
+
+
+    def _forward_pass(self, state: tf.Tensor) -> tf.Tensor:
+        """Compute the probability distribution of actions according to the policy."""
+        l1 = self.conv1(state)
+        l2 = self.conv2(l1)
+        l3 = self.flatten(l2)
+        l4 = self.fc(l3)
+        l5 = self.prob(l4)
+        return l5
+
+
+    def sample_action(self, state: tf.Tensor) -> tf.Tensor:
+        """Sample action according to the policy from a given state."""
+        prob = self._forward_pass(state)
+        return 2 if np.random.uniform() < prob[0][0] else 5
+
+
+    def update(self, reward: float, reset_value: bool, reset_episode: bool):
         pass
 
 
-    def update(reward: float, reset_value: bool, reset_episode: bool):
+    def update_policy(self):
         pass
-
-
-    def update_policy():
-        pass
-
-
-def main():
-    batch_size = 10
-    episode_count = 0
-    agent = Agent()
-    env = gym.make('Pong-v0')
-    
-    while True:
-        observation = env.reset()
-        action = agent.sample_action(observation=observation)
-
-        observation, reward, episode_done, info = env.step(action)
-        episode_count += 1 * episode_done
-
-        agent.update(reward=reward, reset_value=(reward == 1), reset_episode=episode_done)
-
-        if episode_count % batch_size == 0:
-            agent.update_policy()
--- pong.py
+++ pong.py
@@ -1,5 +1,7 @@
 """
-    An implementation of a policy gradient agent that learns to play Atari games.
+    An implementation of a policy gradient agent that learns to play the 
+    mighty game of Pong.
+    
     More Info : https://github.com/mlitb/pong-cnn
     Authors:
         1. Faza Fahleraz https://github.com/ffahleraz
@@ -8,29 +10,64 @@
 """
 
 import gym
+import argparse
+import numpy as np
 import tensorflow as tf
-
 from agent import Agent
 
+tf.enable_eager_execution()
 
-def main():
-    batch_size = 10
+
+# Training hyperparameters
+BATCH_SIZE = 10
+
+
+def preprocess(frame: np.ndarray) -> np.ndarray:
+    """
+        Preprocess 210x160x3 uint8 frame into 1x80x80x1 4D float32 tensor.
+    """
+    frame = frame[35:195]
+    frame = frame[::2,::2,0]
+    frame[(frame == 144) | (frame == 109)] = 0
+    frame[frame != 0] = 1
+    frame = np.expand_dims(frame, 0)
+    frame = np.expand_dims(frame, -1)
+    return tf.convert_to_tensor(frame, dtype=tf.float32)
+
+
+def main(render: bool = False):
+    """
+        Main training loop.
+    """
     episode_count = 0
-    agent = Agent()
+    agent = Agent((80, 80, 1))
     env = gym.make('Pong-v0')
-    
-    while True:
-        observation = env.reset()
-        action = agent.sample_action(observation=observation)
+    observation = env.reset()
 
+    while True:
+        state = preprocess(observation)
+        action = agent.sample_action(state=state)
         observation, reward, episode_done, info = env.step(action)
-        episode_count += 1 * episode_done
+
+        if render:
+            env.render()
 
         agent.update(reward=reward, reset_value=(reward == 1), reset_episode=episode_done)
 
-        if episode_count % batch_size == 0:
+        if episode_done:
+            env.reset()
+            episode_count += 1
+            print('Episode: {}'.format(episode_count))
+
+        if episode_count % BATCH_SIZE == 0:
             agent.update_policy()
+
+    env.close()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Train an RL agent to play the mighty game of Pong.')
+    parser.add_argument('-r', '--render', action="store_true", default=False, help='whether to render the environment or not')
+    args = parser.parse_args()
+    main(render=args.render)
+    
