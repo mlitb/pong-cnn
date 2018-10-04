d260b072986f54ed592d330c8a642373909b963c
Abram Situmorang <abram.perdanaputra@gmail.com>
2018-10-04 08:40:19 +0700

    PolicyGradientAgent forward pass

--- policy_gradient_agent.py
+++ policy_gradient_agent.py
@@ -14,6 +14,7 @@ class PolicyGradientAgent:
     def __init__(self, input_shape: Tuple[int, int]):
         self.conv1 = tf.keras.layers.Conv2D(input_shape=input_shape, filters=16, kernel_size=8, strides=4, activation='relu', data_format="channels_last") # 16 8x8 conv, stride 4
         self.conv2 = tf.keras.layers.Conv2D(filters=32, kernel_size=4, strides=2, activation='relu') # 32 4x4 conv, stride 2
+        self.flatten = tf.keras.layers.Flatten()
         self.fc = tf.keras.layers.Dense(256, activation='relu') # 256
         self.val = tf.keras.layers.Dense(1, activation='sigmoid') # Last layer
 
@@ -21,19 +22,15 @@ class PolicyGradientAgent:
     def _forward_pass(self, state: tf.Tensor) -> tf.Tensor:
         """Forward pass."""
         l1 = self.conv1(state)
-        print(l1.shape)
         l2 = self.conv2(l1)
-        print(l2.shape)
-        l3 = self.fc(l2)
-        print(l3.shape)
-        l4 = self.val(l3)
-        print(l4.shape)
-        return l4
+        l3 = self.flatten(l2)
+        l4 = self.fc(l3)
+        l5 = self.val(l4)
+        return l5
 
     def sample_action(self, state: tf.Tensor) -> tf.Tensor:
         """Sampling action from a given state."""
         prob = self._forward_pass(state)
-        print(prob)
-        return 2 if np.random.uniform() < prob else 5
+        return 2 if np.random.uniform() < prob[0][0] else 5
         
         
